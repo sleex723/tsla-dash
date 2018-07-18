68a155d7c86c85030ee2debcaadd0f942a7849ad
sleex89 <samlee723@gmail.com>
2018-07-18 16:35:30 -0700

    Saving before eject

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LeftDash from './LeftDash/LeftDash';
+
+class App extends Component {
+  render() {
+    return (
+      <LeftDash />
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/LeftDash/LeftDash.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Top from './Top/Top';
+
+class LeftDash extends Component {
+  render() {
+    return(
+      <div>
+        <div>Hello</div>
+        <Top />
+      </div>
+    )
+  }
+}
+
+export default LeftDash;
